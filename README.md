99ea90174f198d86d9aea73e1432e01235e0c6fe
Matthew Nessworthy <matthew.nessworthy@gmail.com>
2022-07-17 14:59:22 +0200

    Initial commit

--- README.md
+++ README.md
@@ -1,2 +1,14 @@
 # ringier-code-style
 RingerSA PHP Code Style
+
+## Installation
+
+```bash
+composer require ringierimu/ringier-code-style
+```
+
+## Usage
+
+```bash
+vendor/bin/ringier-code-style list of files and or folders to fix
+```
---
+++ app/Commands/DefaultCommand.php
@@ -0,0 +1,102 @@
+<?php
+
+namespace App\Commands;
+
+use LaravelZero\Framework\Commands\Command;
+use Symfony\Component\Console\Input\InputArgument;
+use Symfony\Component\Process\Process;
+
+class DefaultCommand extends Command
+{
+    protected $name = 'default';
+
+    protected $description = 'Fix code styling for the given path.';
+
+    protected function configure()
+    {
+        parent::configure();
+
+        $this
+            ->setDefinition(
+                [
+                    new InputArgument('path', InputArgument::IS_ARRAY, 'The path to fix', [(string) getcwd()]),
+                ]
+            );
+    }
+
+    /**
+     * Execute the console command.
+     *
+     * @return mixed
+     */
+    public function handle()
+    {
+        $this->runPint();
+        $this->runPHPCS();
+        $this->runComposerNormalize();
+    }
+
+    protected function runPint()
+    {
+        $configFile = tempnam(sys_get_temp_dir(), "pint");
+        rename($configFile, $configFile .= '.json');
+        file_put_contents($configFile, file_get_contents(base_path() . '/pint.json'));
+
+        $this->info('Running pint on ' . implode(', ', $this->argument('path')));
+        $process = new Process(
+            [
+                'vendor/bin/pint',
+                '--config=' . $configFile,
+                ...$this->argument('path'),
+            ],
+            //            base_path(),
+        );
+        $process->run(); // executes after the command finishes
+
+        dump($process->getErrorOutput());
+
+        echo $process->getOutput();
+    }
+
+    protected function runPHPCS()
+    {
+        $configFile = tempnam(sys_get_temp_dir(), "phpcs");
+        rename($configFile, $configFile .= '.xml');
+        file_put_contents($configFile, file_get_contents(base_path() . '/.phpcs.xml'));
+
+        foreach ($this->argument('path') as $path) {
+            $this->info('Running phpcs on ' . $path);
+            $process = new Process(
+                [
+                    'tools/phpcbf',
+                    '--extensions=php',
+                    '--standard=' . $configFile,
+                    $path,
+                ],
+                //                base_path(),
+            );
+            $process->run(); // executes after the command finishes
+
+            dump($process->getErrorOutput());
+            unlink($configFile);
+
+            echo $process->getOutput();
+        }
+    }
+
+    protected function runComposerNormalize()
+    {
+        $this->info('Running composer normalize');
+        $process = new Process(
+            [
+                'tools/composer-normalize',
+            ],
+            //            base_path(),
+        );
+        $process->run(); // executes after the command finishes
+
+        dump($process->getErrorOutput());
+
+        echo $process->getOutput();
+    }
+}
